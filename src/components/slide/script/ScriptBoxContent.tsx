1f96ae85f2cf33e3fbb56be1e4171c148e7e3627
Andy Hong <chili.tomat0@icloud.com>
2026-01-04 21:43:44 +0900

    design: ScriptBox 여백 조절 (#28)

--- src/components/slide/script/ScriptBoxContent.tsx
+++ src/components/slide/script/ScriptBoxContent.tsx
@@ -12,7 +12,7 @@ export default function ScriptBoxContent() {
   const { updateScript, saveToHistory } = useSlideActions();
 
   return (
-    <div className="h-full bg-white px-4 py-3">
+    <div className="h-full bg-white px-5 py-3">
       <textarea
         value={script}
         onChange={(e) => updateScript(e.target.value)}
