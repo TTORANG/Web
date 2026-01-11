231fe5465ba983001dec6302155a21698f11fd31
Andy Hong <chili.tomat0@icloud.com>
2026-01-11 17:40:33 +0900

    fix: DevTestPage 임포트 (#27)

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { LoginButton, Logo } from '@/components/common';
 import { Gnb } from '@/components/layout/Gnb';
 import { Layout } from '@/components/layout/Layout';
 import { DEFAULT_SLIDE_ID } from '@/constants/navigation';
-import { FdSlidePage, HomePage, InsightPage, SlidePage, VideoPage } from '@/pages';
+import { DevTestPage, FdSlidePage, HomePage, InsightPage, SlidePage, VideoPage } from '@/pages';
 import '@/styles/index.css';
 
 const router = createBrowserRouter([
