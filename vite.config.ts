218a8c3f0f695b12b7116be09c0a46c8366d4acb
Andy Hong <chili.tomat0@icloud.com>
2026-01-30 06:39:17 +0900

    fix: Chrome에서 목 데이터 영상 재생 안되는 문제 수정 (#80)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,23 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    // TODO: Vite에서 mp4 파일에 대해 Accept-Ranges 헤더를 추가하는 임시 플러그인
+    {
+      name: 'add-accept-ranges',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.endsWith('.mp4')) {
+            res.setHeader('Accept-Ranges', 'bytes');
+          }
+          next();
+        });
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
